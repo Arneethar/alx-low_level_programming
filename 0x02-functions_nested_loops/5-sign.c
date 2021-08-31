c75410ee0549651e5b6cb367f051f7f76e793c79
Arneethar <anitamatthewc1@gmail.com>
2021-08-31 08:33:20 -0700

    special symbols

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+/*
+ * if else statement
+ * File: 5-sign.c
+ */
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number
+ * @n: character in use
+ * Description: prints symbol based on return
+ * Return: 1 if the number is greater than zero,
+ *         0 if the number is zero,
+ *        -1 if the number is less than zero.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
