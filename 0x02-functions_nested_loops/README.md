e8b9280e0b074456f042a5e50a08a029a0d048aa
Arneethar <anitamatthewc1@gmail.com>
2021-08-31 06:32:36 -0700

    First commit here

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Contains all the solutions to theses exercises
---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+
+/*
+ * File : holberton.h
+ * contains header file declarations for all functions
+ */
+
+int _putchar(char c);
+
+
+#endif
