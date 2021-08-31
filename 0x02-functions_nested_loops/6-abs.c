5788a02cd34f2c0b6ade92dcb3e96f6e1aeef6ad
Arneethar <anitamatthewc1@gmail.com>
2021-08-31 08:47:51 -0700

    Absolute interger

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+/*
+ * Absolute value of an interger
+ */
+#include "main.h"
+
+/**
+ * _abs - computing the absolute value
+ * @n: printing absolute integer
+ * Return: the absolute value of  integer
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
