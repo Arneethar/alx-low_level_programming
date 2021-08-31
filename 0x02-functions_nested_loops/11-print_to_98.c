e4e66bab2f81067f01f72abbbe01374f2d230687
Arneethar <anitamatthewc1@gmail.com>
2021-08-31 10:11:50 -0700

    natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -23,5 +23,17 @@ void print_to_98(int n)
 			n++;
 		}
 	}
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
 	printf("\n");
 }
