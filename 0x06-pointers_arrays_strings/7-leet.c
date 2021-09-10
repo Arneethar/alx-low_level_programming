7bf1ac17b262b7ec939ecd9be8f03dfcf996d456
Arneethar <anitamatthewc1@gmail.com>
2021-09-10 10:58:53 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,34 +8,21 @@
 
 char *leet(char *s)
 {
-	int i;
+	int i = 0;
+	int j = 0;
+	char *l = "aAeEoOlLtT";
+	char *n = "4433001177";
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*(s + i) != '\0')
 	{
-		if (s[i] == 'a' || s[i] == 'A')
+		while (*(l + j) != '\0')
 		{
-			s[i] = '4';
+			if (*(s + i) == *(l + j))
+				*(s + i)  = *(n + j);
+			j++;
 		}
-		else if (s[i] == 'e' || s[i] == 'E')
-		{
-			s[i] = '3';
-		}
-		else if (s[i] == 'o' || s[i] == 'O')
-		{
-			s[i] = '0';
-		}
-		else if (s[i] == 't' || s[i] == 'T')
-		{
-			s[i] = '7';
-		}
-		else if (s[i] == 'l' || s[i] == 'L')
-		{
-			s[i] = '1';
-		}
-		else
-		{
-			(s[i] = s[i]);
-		}
+		j = 0;
+		i++;
 	}
 	return (s);
 }
