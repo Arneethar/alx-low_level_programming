bd3e22474339a38960a652788a77f2dc35744510
Arneethar <anitamatthewc1@gmail.com>
2021-12-20 01:03:58 -0800

    print-table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, flag = 1;
+	hash_node_t *tmp;
+
+	if (ht)
+	{
+		printf("{");
+		for (i = 0; i < ht->size; i++)
+		{
+			tmp = ht->array[i];
+			if (tmp == NULL)
+				continue;
+			else if (flag)
+			{
+				flag = 0;
+				printf("'%s': '%s'", tmp->key,
+				       tmp->value);
+			}
+			else
+			{
+				printf(", '%s': '%s'", tmp->key,
+				       tmp->value);
+			}
+			tmp = tmp->next;
+			while (tmp)
+			{
+				printf(", '%s': '%s'", tmp->key, tmp->value);
+				tmp = tmp->next;
+			}
+		}
+		printf("}\n");
+	}
+}
